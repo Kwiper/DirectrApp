0bca14a13c62c86d051faaa23671285eefe3b04f
Jonathan Newman <jonny.tibboh@gmail.com>
2026-04-18 16:06:12 -0700

    Update firebaseConfig.js
    Removed firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdmfRzRlYAaXFa_BPG5sCoe-qZyCc-5No",
-  authDomain: "moverecapp.firebaseapp.com",
-  projectId: "moverecapp",
-  storageBucket: "moverecapp.firebasestorage.app",
-  messagingSenderId: "572156967563",
-  appId: "1:572156967563:web:357b39f251512cf146f28e"
+
 };
 
 // Initialize Firebase
